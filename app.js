6b50d51ff082458bde645fd4965b3bec9e5e5fa8
Cristian Hernandez <chinorubikguitar@hotmail.com>
2022-07-27 07:30:36 -0500

    trying to change moonsoge bt mysql

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-'use strict'
+"use strict";
 
-//importaciones
-var express = require('express');
-var bodyParse = require('body-parser');
+//IMPORTS
+const express = require("express");
+const bodyParse = require("body-parser");
+const mysql = require('mysql');
+const express_myconnection = require('express-myconnection');
+const app = express();
+require('dotenv').config();
 
-//lanzar el servicio
-var app = express();
+const db = {
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASSWORD_DB,
+    database: process.env.DATABASE_DB,
+};
 
-//middlewares
-app.use(bodyParse.urlencoded({extended : false}));
+//MIDDLEWARES
+app.use(bodyParse.urlencoded({ extended: false }));
 app.use(bodyParse.json());
 
-//cors
+//CONNECTION TO DATABASE
+app.use(express_myconnection(mysql, db, 'single'))
+
+//CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
-//rutas
-
-var routes = require('./routes/routes');
+//ROUTES
+const routes = require("./routes/routes");
 app.use(routes);
 
-//exportarlo
+//EXPORTATION
 module.exports = app;
--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-'use strict'
+"use strict";
 
-var mongoose = require('mongoose');
-var app = require('./app');
-var port=3500;
+const app = require("./app");
+const port = 3500;
 
-mongoose.Promise = global.Promise;
-
-//Connection to DB
-
-mongoose.connect("mongodb://localhost:27017/piano-hobby")
-    .then(res =>{
-        console.log("la conexion a la base de datos exitosa");
-        app.listen(port,() =>{
-            console.log("El servicio se ha levantado correctamente en mi http://localhost:"+port);
-        })
-    })
-    .catch(err =>{
-        console.log(err);
-    });
+app.listen(port, () => {
+  console.log(`Servidor corriendo en mi http://localhost:${port}`);
+});
