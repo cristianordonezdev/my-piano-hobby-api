b3e9950a347159c7c8006a83eed5a9f341cd4708
Cristian Hernandez <chinorubikguitar@hotmail.com>
2022-09-29 08:21:53 -0500

    usiing port in env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const app = require("./app");
-const port = 3500;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en mi http://localhost:${port}`);
